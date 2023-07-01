30c61f37d5c6f43bb8f0f82fea66083808f57c35
龙飞 谭 <761346811@qq.com>
2023-07-01 18:58:04 +0800

    添加注释

--- maliap/LevelManager.cpp
+++ maliap/LevelManager.cpp
@@ -1,221 +1,232 @@
 #include "LevelManager.h"
-// 通用方法
+
+// 显示碰撞区域，参数为碰撞区域的位置和颜色
 void ShowCollide(Utils::Rect address, int color)
 {
-	setlinecolor(color);
+	setlinecolor(color);  // 设置线条颜色
 	rectangle((int)address.posx, (int)address.posy,
 		(int)address.posx + (int)address.width,
-		(int)address.posy + (int)address.height);
-	setlinecolor(WHITE);
+		(int)address.posy + (int)address.height);  // 绘制矩形，表示碰撞区域
+	setlinecolor(WHITE);  // 将线条颜色设置回白色
 }
 
+// 通知UI，参数为UI对象，是否有消息，以及消息内容
 void NoticeUI(MonoObject* obj, bool hasMessage, ExMessage msg)
 {
-	if (hasMessage) obj->onGetMessage(msg);
-	obj->onFrameUpdate();
+	if (hasMessage) obj->onGetMessage(msg);  // 如果有消息，则调用对象的onGetMessage方法处理消息
+	obj->onFrameUpdate();  // 调用对象的onFrameUpdate方法更新帧
 }
-// LevelManager.cpp
 
+// 获取LevelManager的单例
 LevelManager& LevelManager::GetInstance()
 {
-	static LevelManager m_Instance;
-	return m_Instance;
+	static LevelManager m_Instance;  // 定义一个静态的LevelManager实例
+	return m_Instance;  // 返回这个实例
 }
 
+// 添加对象，参数为要添加的对象
 void LevelManager::AddObject(MonoObject* obj)
 {
-	m_activeObjects.insert(obj);
+	m_activeObjects.insert(obj);  // 将对象添加到活动对象集合中
 }
 
+// 删除对象，参数为要删除的对象
 void LevelManager::DeleteObject(MonoObject* obj)
 {
-	m_deletingObjects.push(obj);
+	m_deletingObjects.push(obj);  // 将对象添加到待删除对象队列中
 }
 
-
-
-
+// 运行LevelManager
 void LevelManager::Run()
 {
-	ClearDeletingObjects();
-	ExMessage msg;
-	bool hasMessage = peekmessage(&msg);
-	set<MonoObject*> UIObjects;
-	ProcessActiveObjects(hasMessage, msg, UIObjects);
-	noticeMove();
-	noticeGravity();
-	ProcessUIObjects(hasMessage, msg, UIObjects);
+	ClearDeletingObjects();  // 清除待删除的对象
+	ExMessage msg;  // 定义一个消息
+	bool hasMessage = peekmessage(&msg);  // 检查是否有消息
+	set<MonoObject*> UIObjects;  // 定义一个UI对象集合
+	ProcessActiveObjects(hasMessage, msg, UIObjects);  // 处理活动对象
+	noticeMove();  // 通知移动效果
+	noticeGravity();  // 通知重力效果
+	ProcessUIObjects(hasMessage, msg, UIObjects);  // 最后处理UI对象
 }
-
+// 清除待删除的对象
 void LevelManager::ClearDeletingObjects()
 {
-	while (!m_deletingObjects.empty())
+	while (!m_deletingObjects.empty())  // 当待删除对象队列不为空时
 	{
-		MonoObject* obj = m_deletingObjects.front();
-		m_deletingObjects.pop();
-		m_activeObjects.erase(obj);
+		MonoObject* obj = m_deletingObjects.front();  // 获取队列中的第一个对象
+		m_deletingObjects.pop();  // 从队列中移除这个对象
+		m_activeObjects.erase(obj);  // 从活动对象集合中删除这个对象
 	}
 }
 
+// 处理活动对象，参数为是否有消息，消息内容，以及UI对象集合
 void LevelManager::ProcessActiveObjects(bool hasMessage, ExMessage& msg, set<MonoObject*>& UIObjects)
 {
-	for (auto& obj : m_activeObjects) {
-		if (obj->type == UI)
+	for (auto& obj : m_activeObjects) {  // 遍历活动对象集合
+		if (obj->type == UI)  // 如果对象类型为UI
 		{
-			UIObjects.insert(obj);
-			continue;
+			UIObjects.insert(obj);  // 将对象添加到UI对象集合中
+			continue;  // 跳过当前循环，处理下一个对象
 		}
-		if (hasMessage) noticeMessage(obj, &msg);
-		obj->onFrameUpdate();
-		if (obj->vx != 0 || obj->vy != 0) m_moveingObjects.push(obj);
-		if (obj->useGravity)
+		if (hasMessage) noticeMessage(obj, &msg);  // 如果有消息，则通知对象处理消息
+		obj->onFrameUpdate();  // 调用对象的onFrameUpdate方法更新帧
+		if (obj->vx != 0 || obj->vy != 0) m_moveingObjects.push(obj);  // 如果对象有速度，则将对象添加到移动对象队列中
+		if (obj->useGravity)  // 如果对象受重力影响
 		{
-			m_fallingObjects[obj] += Gravity / Sceneconfig::GetInstance()->MaxFrame;
+			m_fallingObjects[obj] += Gravity / Sceneconfig::GetInstance()->MaxFrame;  // 更新对象的下落速度
 		}
-		else m_fallingObjects.erase(obj);
-		if (debugModel)
+		else m_fallingObjects.erase(obj);  // 否则，从下落对象集合中删除这个对象
+		if (debugModel)  // 如果处于调试模式
 		{
-			if (!obj->canThrough) ShowCollide(obj->body, RED);
-			else ShowCollide(obj->body, GREEN);
+			if (!obj->canThrough) ShowCollide(obj->body, RED);  // 如果对象不能穿透，则显示红色的碰撞区域
+			else ShowCollide(obj->body, GREEN);  // 否则，显示绿色的碰撞区域
 		}
 	}
 }
 
+// 处理UI对象，参数为是否有消息，消息内容，以及UI对象集合
 void LevelManager::ProcessUIObjects(bool hasMessage, ExMessage& msg, set<MonoObject*>& UIObjects)
 {
-	for (auto& obj : UIObjects)
+	for (auto& obj : UIObjects)  // 遍历UI对象集合
 	{
-		NoticeUI(obj, hasMessage, msg);
+		NoticeUI(obj, hasMessage, msg);  // 通知UI对象处理消息和更新帧
 	}
 }
+
+// 通知消息，参数为要通知的对象，以及消息内容
 void LevelManager::noticeMessage(MonoObject* obj, ExMessage* msg)
 {
-	obj->onGetMessage(*msg);
+	obj->onGetMessage(*msg);  // 调用对象的onGetMessage方法处理消息
 }
-
+// 通知移动
 void LevelManager::noticeMove()
 {
-	while (!m_moveingObjects.empty())
+	while (!m_moveingObjects.empty())  // 当移动对象队列不为空时
 	{
-		MonoObject* obj = m_moveingObjects.front();
-		m_moveingObjects.pop();
-		processObjectMovement(obj);
+		MonoObject* obj = m_moveingObjects.front();  // 获取队列中的第一个对象
+		m_moveingObjects.pop();  // 从队列中移除这个对象
+		processObjectMovement(obj);  // 处理对象的移动
 	}
-	noticeCrash();
+	noticeCrash();  // 处理并通知碰撞效果
 }
 
+// 处理对象的移动，参数为要移动的对象
 void LevelManager::processObjectMovement(MonoObject* obj)
 {
-	float x = obj->vx / Sceneconfig::GetInstance()->MaxFrame;
-	float y = obj->vy / Sceneconfig::GetInstance()->MaxFrame;
-	Utils::Rect address = Utils::Rect{ obj->body.posx + x,obj->body.posy + y ,obj->body.width,obj->body.height };
-	vector<MonoObject*> crashObjs = caculateCrash(address);
-	MonoObject* crashObj_x = findCrashObject(crashObjs, obj, address, true);
-	MonoObject* crashObj_y = findCrashObject(crashObjs, obj, address, false);
-	processCrashObjects(obj, crashObj_x, crashObj_y);
-	if (crashObj_x == nullptr && crashObj_y == nullptr)
+	float x = obj->vx / Sceneconfig::GetInstance()->MaxFrame;  // 计算对象在x方向上的移动距离
+	float y = obj->vy / Sceneconfig::GetInstance()->MaxFrame;  // 计算对象在y方向上的移动距离
+	Utils::Rect address = Utils::Rect{ obj->body.posx + x,obj->body.posy + y ,obj->body.width,obj->body.height };  // 计算对象移动后的位置
+	vector<MonoObject*> crashObjs = caculateCrash(address);  // 计算碰撞的对象
+	MonoObject* crashObj_x = findCrashObject(crashObjs, obj, address, true);  // 找到在x方向上碰撞的对象
+	MonoObject* crashObj_y = findCrashObject(crashObjs, obj, address, false);  // 找到在y方向上碰撞的对象
+	processCrashObjects(obj, crashObj_x, crashObj_y);  // 处理碰撞的对象
+	if (crashObj_x == nullptr && crashObj_y == nullptr)  // 如果没有碰撞的对象
 	{
 		Utils::Rect position = obj->body;
 		position.posx += x;
 		position.posy += y;
-		obj->body = position;
-		obj->onMoveSuccess();
+		obj->body = position;  // 更新对象的位置
+		obj->onMoveSuccess();  // 调用对象的onMoveSuccess方法
 	}
 }
 
+// 找到碰撞的第一个对象，参数为碰撞的对象集合，目标对象，目标对象的位置，碰撞的方向
 MonoObject* LevelManager::findCrashObject(vector<MonoObject*>& crashObjs, MonoObject* targetObj, Utils::Rect& address, bool isXDirection)
 {
-	MonoObject* crashObj = nullptr;
-	for (MonoObject* it : crashObjs)
+	MonoObject* crashObj = nullptr;  // 初始化碰撞的对象为null
+	for (MonoObject* it : crashObjs)  // 遍历碰撞的对象集合
 	{
-		if (it->GetHashID() == targetObj->GetHashID()) continue;
-		if (crashObj == nullptr ||
-			abs((isXDirection ? crashObj->body.posx : crashObj->body.posy) - (isXDirection ? address.posx : address.posy))
-			< abs((isXDirection ? it->body.posx : it->body.posy) - (isXDirection ? address.posx : address.posy)))
+		if (it->GetHashID() == targetObj->GetHashID()) continue;  // 如果对象的哈希ID等于目标对象的哈希ID，则跳过当前循环
+		if (crashObj == nullptr ||  // 如果碰撞的对象为null，或者
+			abs((isXDirection ? crashObj->body.posx : crashObj->body.posy) - (isXDirection ? address.posx : address.posy))  // 碰撞的对象在对应方向上的位置与目标对象的位置的差的绝对值
+			< abs((isXDirection ? it->body.posx : it->body.posy) - (isXDirection ? address.posx : address.posy)))  // 小于当前对象在对应方向上的位置与目标对象的位置的差的绝对值
 		{
-			crashObj = it;
+			crashObj = it;  // 则更新碰撞的对象为当前对象
 		}
 	}
-	return crashObj;
+	return crashObj;  // 返回碰撞的对象
 }
 
+// 处理碰撞的对象，参数为在x方向和y方向上碰撞到的对象
 void LevelManager::processCrashObjects(MonoObject* obj, MonoObject* crashObj_x, MonoObject* crashObj_y)
 {
-	if (crashObj_x != nullptr)
+	if (crashObj_x != nullptr)  // 如果在x方向上有碰撞的对象
 	{
-		m_crashObjects[make_pair(obj, crashObj_x)]=true;
-		if (!obj->canThrough) m_crashObjects[make_pair(crashObj_x, obj)]=true;
+		// 判断谁撞到了谁
+		m_crashObjects[make_pair(obj, crashObj_x)] = true;  
+		if (!obj->canThrough) m_crashObjects[make_pair(crashObj_x, obj)] = true;  
 	}
-	if (crashObj_y != nullptr)
+	if (crashObj_y != nullptr)  // 如果在y方向上有碰撞的对象
 	{
-		m_crashObjects[make_pair(obj, crashObj_y)]=true;
-		if (!obj->canThrough) m_crashObjects[make_pair(crashObj_y, obj)]=true;
+		m_crashObjects[make_pair(obj, crashObj_y)] = true;  // 则将对象和碰撞的对象添加到碰撞对象集合中
+		if (!obj->canThrough) m_crashObjects[make_pair(crashObj_y, obj)] = true;  // 如果对象不能穿透，则将碰撞的对象和对象添加到碰撞对象集合中
 	}
 }
 
+// 计算碰撞，参数为一个矩形区域
 vector<MonoObject*> LevelManager::caculateCrash(Utils::Rect address)
 {
-	vector<MonoObject*> crashObjs;
-	for (MonoObject* obj : m_activeObjects) {
-		if (!obj->canThrough && Utils::CrossLine(obj->body, address))
+	vector<MonoObject*> crashObjs;  // 初始化碰撞对象集合
+	for (MonoObject* obj : m_activeObjects) {  // 遍历活动对象集合
+		if (!obj->canThrough && Utils::CrossLine(obj->body, address))  // 如果对象不能穿透，并且对象的体积与给定的矩形区域有交叉
 		{
-			crashObjs.push_back(obj);
+			crashObjs.push_back(obj);  // 则将对象添加到碰撞对象集合中
 		}
 	}
-	return crashObjs;
+	return crashObjs;  // 返回碰撞对象集合
 }
 
+// 获取所有对象
 vector<MonoObject*> LevelManager::getAllObjects()
 {
-	vector<MonoObject*> rev;
-	for (MonoObject* obj : m_activeObjects) {
-		rev.push_back(obj);
+	vector<MonoObject*> rev;  // 初始化返回的对象集合
+	for (MonoObject* obj : m_activeObjects) {  // 遍历活动对象集合
+		rev.push_back(obj);  // 将对象添加到返回的对象集合中
 	}
-	return rev;
+	return rev;  // 返回对象集合
 }
 
+// 通知碰撞
 void LevelManager::noticeCrash()
 {
-	for (auto& pair : m_crashObjects) {
-		pair.first.first->onCrash(pair.first.second);
+	for (auto& pair : m_crashObjects) {  // 遍历碰撞对象集合
+		pair.first.first->onCrash(pair.first.second);  // 调用对象的onCrash方法处理碰撞
 	}
-	m_crashObjects.clear();
+	m_crashObjects.clear();  // 清空碰撞对象集合
 }
 
+// 通知重力
 void LevelManager::noticeGravity()
 {
 	// 遍历下坠物体
 	map<MonoObject*, float>::iterator it;
-	for (it = m_fallingObjects.begin(); it != m_fallingObjects.end(); it++) {
+	for (it = m_fallingObjects.begin(); it != m_fallingObjects.end(); it++) {  
 		// 计算当前帧重力速度导致的位移
-		MonoObject* obj = it->first;
-		float y = it->second / Sceneconfig::GetInstance()->MaxFrame;
-		// 无碰撞情况运动位置
-		Utils::Rect address = Utils::Rect{ obj->body.posx,obj->body.posy + y
-			,obj->body.width,obj->body.height };
-		vector<MonoObject*> crashObjs = caculateCrash(address);
-		// 计算重力触碰到的对象
-		MonoObject* crashObj = findCrashObjectByGravity(crashObjs, obj, address);
-		// 通知重力触碰到的对象
-		if (crashObj != nullptr)
+		MonoObject* obj = it->first;  // 获取下坠物体
+		float y = it->second / Sceneconfig::GetInstance()->MaxFrame;  // 计算下坠物体在y方向上的位移
+		Utils::Rect address = Utils::Rect{ obj->body.posx,obj->body.posy + y ,obj->body.width,obj->body.height };  // 计算下坠物体移动后的位置
+		vector<MonoObject*> crashObjs = caculateCrash(address);  // 计算碰撞的对象
+		MonoObject* crashObj = findCrashObjectByGravity(crashObjs, obj, address);  // 找到由重力引起碰撞的对象
+		if (crashObj != nullptr)  // 如果有碰撞的对象
 		{
-			it->second = 0;
-			obj->onGroud(crashObj);
+			it->second = 0;  // 则将下坠物体在y方向上的重力速度设置为0
+			obj->onGroud(crashObj);  // 调用下坠物体的onGroud方法处理碰撞地面
 		}
-		else obj->body = address;
+		else obj->body = address;  // 否则，更新下坠物体的位置
 	}
 }
 
+// 找到由重力引起的碰撞的对象，参数为碰撞的对象集合，下坠物体，以及下坠物体的位置
 MonoObject* LevelManager::findCrashObjectByGravity(vector<MonoObject*>& crashObjs, MonoObject* obj, Utils::Rect& address)
 {
-	MonoObject* crashObj = nullptr;
-	for (MonoObject* it : crashObjs)
+	MonoObject* crashObj = nullptr;  // 初始化碰撞的对象为null
+	for (MonoObject* it : crashObjs)  // 遍历碰撞的对象集合
 	{
-		if (it->GetHashID() == obj->GetHashID()) continue;
-		if (crashObj == nullptr || abs(crashObj->body.posy - address.posy) < abs(it->body.posy - address.posy))
+		if (it->GetHashID() == obj->GetHashID()) continue;  // 如果对象的哈希ID等于下坠物体的哈希ID，则跳过当前循环
+		if (crashObj == nullptr || abs(crashObj->body.posy - address.posy) < abs(it->body.posy - address.posy))  // 如果碰撞的对象为null，或者碰撞的对象在y方向上的位置与下坠物体的位置的差的绝对值小于当前对象在y方向上的位置与下坠物体的位置的差的绝对值
 		{
-			crashObj = it;
+			crashObj = it;  // 则更新碰撞的对象为当前对象
 		}
 	}
-	return crashObj;
+	return crashObj;  // 返回碰撞的对象
 }
--- maliap/LevelManager.h
+++ maliap/LevelManager.h
@@ -11,52 +11,80 @@
 #include "SceneConfig.h"
 using namespace std;
 
+// 这段代码定义了一个名为LevelManager的类，该类用于管理游戏中的各种对象和事件。
+//这段代码主要包含了游戏中的对象管理、碰撞检测、对象移动等功能。 
+// 定义LevelManager类
 class LevelManager
 {
 public:
+    // 获取LevelManager的单例
     static LevelManager& GetInstance();
+    // 运行LevelManager
     void Run();
+    // 计算碰撞
     vector<MonoObject*> caculateCrash(Utils::Rect address);
+    // 获取所有对象
     vector<MonoObject*> getAllObjects();
 private:
+    // 默认构造函数
     LevelManager() = default;
+    // 删除复制构造函数
     LevelManager(const LevelManager&) = delete;
+    // 删除赋值运算符
     LevelManager& operator=(const LevelManager&) = delete;
-    // 物体管理
+    // 添加对象
     void AddObject(MonoObject* obj);
+    // 准备删除对象
     void DeleteObject(MonoObject* obj);
-    // Run函数重构
+    // 清除待删除的对象的物理内存
     void ClearDeletingObjects();
+    // 处理活动对象
     void ProcessActiveObjects(bool hasMessage, ExMessage& msg, set<MonoObject*>& UIObjects);
+    // 处理UI对象
     void ProcessUIObjects(bool hasMessage, ExMessage& msg, set<MonoObject*>& UIObjects);
-
+    // 通知玩家键盘或者鼠标信息
     void noticeMessage(MonoObject* obj, ExMessage* msg);
     // 处理移动逻辑
     void noticeMove();
+    // 处理对象移动结果
     void processObjectMovement(MonoObject* obj);
+    // 查找碰撞对象
     MonoObject* findCrashObject(vector<MonoObject*>& crashObjs, MonoObject* targetObj, Utils::Rect& address, bool isXDirection);
+    // 处理碰撞对象
     void processCrashObjects(MonoObject* obj, MonoObject* crashObj_x, MonoObject* crashObj_y);
 
     // 通知碰撞或者重力效果
     void noticeCrash();
+    // 查找重力碰撞对象
     MonoObject* findCrashObjectByGravity(vector<MonoObject*>& crashObjs, MonoObject* obj, Utils::Rect& address);
+    // 通知重力
     void noticeGravity();
 
-    bool debugModel = false;
+    // 调试模式
+    const bool debugModel = false;
+    // 哈希ID
     int hashID = 0;
-    float Gravity = 200;
+    // 重力值
+    const float Gravity = 200;
+    // 待删除的对象队列
     queue<MonoObject*> m_deletingObjects;
+    // 活动对象集合
     set<MonoObject*> m_activeObjects;
+    // 移动对象队列
     queue<MonoObject*> m_moveingObjects;
+    // 下落对象映射
     map<MonoObject*, float> m_fallingObjects;
-    map<MonoObject*, pair<float, float>> m_viewingObjects;
+    // 碰撞对象映射
     map < pair<MonoObject*, MonoObject*>, bool> m_crashObjects;
 
+    // 创建对象的模板函数
     template <typename T>
     friend inline T* CreateObject(Utils::Rect body);
+    // 销毁对象的函数
     friend inline void DestroyObject(MonoObject* destroyObject);
 };
 
+// 创建对象的模板函数实现
 template <typename T>
 inline T* CreateObject(Utils::Rect body)
 {
@@ -68,6 +96,7 @@ inline T* CreateObject(Utils::Rect body)
     return newObject;
 }
 
+// 销毁对象的函数实现
 inline void DestroyObject(MonoObject* destroyObject)
 {
     LevelManager::GetInstance().DeleteObject(destroyObject);
